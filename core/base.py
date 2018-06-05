cb24a1fa691aa87705fb1fbb6e870e10e0e49545
Luiz <luizrodrigo46@hotmail.com>
2018-06-05 09:49:40 -0300

    app instance moved to base.py

--- core/base.py
+++ core/base.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from flask import Flask
+from flask_marshmallow import Marshmallow
 from settings import DB_URL
-
 
 engine = create_engine(DB_URL)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
+
+app = Flask(__name__)
+ma = Marshmallow(app)
