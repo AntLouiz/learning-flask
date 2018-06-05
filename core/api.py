7a2bcbb5516a59db6bf165cf83fa86260d92e29a
Luiz <luizrodrigo46@hotmail.com>
2018-06-05 19:31:04 -0300

    post method added to the cities resource

--- core/api.py
+++ core/api.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource, Api
-from models.city import City
+from flask import request, jsonify
+from models.city import City, city_schema, cities_schema
 from base import create_app, db
 
 app = create_app()
@@ -10,11 +11,21 @@ db.create_all(app=app)
 class CityResource(Resource):
     def get(self):
         cities = City.query.all()
-        print(cities)
-        return {'hello': 'world'}
+        result = cities_schema.dump(cities)
+
+        return jsonify(result.data)
+
+    def post(self):
+        new_city = City(request.form['name'], request.form['uf'])
+        db.session.add(new_city)
+        db.session.commit()
+
+        result = city_schema.dump(new_city)
+
+        return jsonify(result.data)
 
 
-api.add_resource(CityResource, '/')
+api.add_resource(CityResource, '/cities', methods=['GET', 'POST'])
 
 if __name__ == '__main__':
     app.run(debug=True)
