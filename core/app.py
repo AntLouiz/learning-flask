ab360c68ac779fe670e5686aaa962363fea94409
Luiz <luizrodrigo46@hotmail.com>
2018-06-05 10:13:24 -0300

    add city endpoint added

--- core/app.py
+++ core/app.py
@@ -1,3 +1,4 @@
+from flask import request, jsonify
 from base import Base, Session, engine, app
 from bases.city import City, CitySchema
 
@@ -19,6 +20,21 @@ def list_cities():
     city_schema = CitySchema()
 
     return city_schema.jsonify(cities)
+
+
+@app.route('/cities', methods=['POST'])
+def add_cities():
+    city_name = request.form['name']
+    city_uf = request.form['uf']
+
+    new_city = City(city_name, city_uf)
+    city_schema = CitySchema()
+
+    session.add(new_city)
+
+    session.commit()
+
+    return city_schema.jsonify(new_city)
 
 
 if __name__ == '__main__':
