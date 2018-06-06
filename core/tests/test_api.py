0f380b4f585439b4bc057b588e8a64992b582f34
Luiz <luizrodrigo46@hotmail.com>
2018-06-06 09:46:20 -0300

    test_get_cities_many_data added

--- core/tests/test_api.py
+++ core/tests/test_api.py
@@ -19,3 +19,22 @@ def test_get_cities_data(client):
 
         assert len(data) == 1
         assert data[0]['name'] == 'Parnaíba'
+
+
+def test_get_cities_many_data(client):
+    with client:
+        cities = [
+            {'name': 'Fortaleza', 'uf': 'CE'},
+            {'name': 'Teresina', 'uf': 'PI'},
+            {'name': 'Joao Pessoa', 'uf': 'PB'}
+        ]
+        cities = City.save_all(cities)
+
+        response = client.get('/cities')
+
+        data = json.loads(response.data)
+
+        assert len(data) == 3
+        assert data[0]['slug'] == 'fortaleza'
+        assert data[1]['slug'] == 'teresina'
+        assert data[2]['slug'] == 'joao-pessoa'
