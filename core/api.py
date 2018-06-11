3d9ba8ee2488948eb65a95299efd81f528881455
Luiz <luizrodrigo46@hotmail.com>
2018-06-11 09:49:35 -0300

    make the imports on top of file

--- core/api.py
+++ core/api.py
@@ -1,12 +1,11 @@
 from flask_heroku import Heroku
 from flask_restful import Api
 from core.base import db, app
+from resources import CityResource
 
 
 heroku = Heroku(app)
 db.create_all()
 api = Api(app)
-
-from resources import CityResource
 
 api.add_resource(CityResource, '/cities', methods=['GET', 'POST'])
