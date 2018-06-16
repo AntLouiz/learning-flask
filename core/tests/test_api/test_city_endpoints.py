dfc245ac421777334d8559f2b648e315151a4f8e
Luiz <luizrodrigo46@hotmail.com>
2018-06-15 22:43:38 -0300

    test modules refactored

---
+++ core/tests/test_api/test_city_endpoints.py
@@ -0,0 +1,99 @@
+import json
+import pytest
+from core.models.city import City
+
+
+@pytest.mark.resources
+def test_get_cities(client):
+    response = client.get('/cities', headers=client.auth_header)
+    assert response.status_code == 200
+
+
+@pytest.mark.resources
+def test_get_cities_data(client):
+    new_city = City('Parnaíba', 'PI')
+    new_city.save()
+
+    response = client.get('/cities', headers=client.auth_header)
+
+    data = json.loads(response.data)
+
+    assert len(data) == 1
+    assert data[0]['name'] == 'Parnaíba'
+
+
+@pytest.mark.resources
+def test_get_cities_many_data(client):
+    cities = [
+        {'name': 'Fortaleza', 'uf': 'CE'},
+        {'name': 'Teresina', 'uf': 'PI'},
+        {'name': 'Joao Pessoa', 'uf': 'PB'}
+    ]
+    cities = City.save_all(cities)
+
+    response = client.get('/cities', headers=client.auth_header)
+
+    data = json.loads(response.data)
+
+    assert len(data) == 3
+    assert data[0]['slug'] == 'fortaleza'
+    assert data[1]['slug'] == 'teresina'
+    assert data[2]['slug'] == 'joao-pessoa'
+
+
+@pytest.mark.resources
+def test_post_city_data(client):
+    city = {'name': 'Fortaleza', 'uf': 'CE'}
+    response = client.post('/cities', data=city, headers=client.auth_header)
+    city = City.query.filter_by(slug='fortaleza').first()
+
+    assert response.status_code == 201
+    assert city.name == 'Fortaleza'
+
+
+@pytest.mark.resources
+def test_post_many_cities(client):
+    cities = [
+        {'name': 'Fortaleza', 'uf': 'CE'},
+        {'name': 'Teresina', 'uf': 'PI'},
+        {'name': 'Joao Pessoa', 'uf': 'PB'}
+    ]
+
+    response = client.post('/cities', data=cities[0], headers=client.auth_header)
+    city = City.query.filter_by(name=cities[0]['name']).first()
+
+    assert response.status_code == 201
+    assert city.name == cities[0]['name']
+
+    response = client.post('/cities', data=cities[1], headers=client.auth_header)
+    city = City.query.filter_by(name=cities[1]['name']).first()
+
+    assert response.status_code == 201
+    assert city.name == cities[1]['name']
+
+    response = client.post('/cities', data=cities[2], headers=client.auth_header)
+    city = City.query.filter_by(name=cities[2]['name']).first()
+
+    assert response.status_code == 201
+    assert city.name == cities[2]['name']
+
+
+@pytest.mark.resources
+def test_post_cities_with_same_name(client):
+    city = {'name': 'Fortaleza', 'uf': 'CE'}
+
+    response = client.post('/cities', data=city, headers=client.auth_header)
+
+    assert response.status_code == 201
+
+    response = client.post('/cities', data=city, headers=client.auth_header)
+    data = json.loads(response.data)
+
+    """
+        409 Conflict: Indicates that the request could not 
+        be processed because of conflict in the request, such 
+        as an edit conflict.
+    """
+
+    assert response.status_code == 409
+    assert data['message'] == 'This city already exists.'
--- core/tests/test_models/test_city_model.py
+++ core/tests/test_models/test_city_model.py
@@ -1,6 +1,6 @@
 import pytest
 import sqlalchemy
-from core.models.city import City
+from core.models.city import City, city_schema, cities_schema
 
 
 def test_create_new_city(client):
@@ -101,3 +101,27 @@ def test_delete_city_on_database(client):
     count_search_city = City.query.filter_by(name='Timon').count()
 
     assert count_search_city == 0
+
+
+def test_city_schema(client):
+    new_city = City(name='Parnaíba', uf='PI')
+    new_city.save()
+
+    result = city_schema.dump(new_city)
+
+    assert result.data == {'name': 'Parnaíba', 'uf': 'PI', 'slug': 'parnaiba'}
+
+
+def test_cities_schema(client):
+    cities = [
+        {'name': 'Pedro II', 'uf': 'PI'},
+        {'name': 'Brasília', 'uf': 'DF'},
+        {'name': 'Campina Grande', 'uf': 'PB'}
+    ]
+    cities = City.save_all(cities)
+
+    result = cities_schema.dump(cities)
+
+    assert result.data[0] == {'name': 'Pedro II', 'uf': 'PI', 'slug': 'pedro-ii'}
+    assert result.data[1] == {'name': 'Brasília', 'uf': 'DF', 'slug': 'brasilia'}
+    assert result.data[2] == {'name': 'Campina Grande', 'uf': 'PB', 'slug': 'campina-grande'}
